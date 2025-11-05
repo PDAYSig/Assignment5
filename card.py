6ba8fe5449e7ce8e5ac8827477b3e6d9a2a378c7
PDAYSig <patrikdagur@gmail.com>
2025-11-05 16:16:01 +0000

    added constants and face card logic

--- card.py
+++ card.py
@@ -1,34 +1,35 @@
 from typing import Union
 class Card:
+    ACE_VALUE = 15 # Since the ace has a different value in different games, We make its value a constant for scalability
+
+    FACE_CARDS = {
+        'J' : 11,
+        'Q' : 12,
+        'K' : 13,
+        'A' : ACE_VALUE
+        }
     def __init__(self, rank : Union[str, int], suit : str):
-        # super().__init__()
         self.suit = suit
         '''If we cannot make the rank an integer, it must mean that the card is
             a Jack, Queen, King, or Ace so we must assign them values specially'''
+
         try:
             self.rank = int(rank)
-        except:
-            if rank == 'J':
-                self.rank = 11
-            elif rank == 'Q':
-                self.rank = 12
-            elif rank == 'K':
-                self.rank = 13
-            elif rank == 'A':
-                self.rank = 14
-            
+        except ValueError:
+            if rank in Card.FACE_CARDS:
+                self.rank = Card.FACE_CARDS[rank]
+
 
     def __str__(self):
-        if self.rank == 11:
-            print_rank = 'J'
-        elif self.rank == 12:
-            print_rank = 'Q'
-        elif self.rank == 13:
-            print_rank = 'K'
-        elif self.rank == 14:
-            print_rank = 'A'
+        if self.rank >= 11:
+            target_rank = self.rank
+            for letter_value, numerical_value in Card.FACE_CARDS.items():
+                if numerical_value == target_rank:
+                    print_rank = letter_value
         else:
             print_rank = self.rank
 
         str_return = f'{print_rank}{self.suit}'
-        return f'{str_return :>3}'
+        return str_return.rjust(3)
+
+
--- hand.py
+++ hand.py
@@ -1,50 +1,41 @@
-# (6 points) __init__(): An initializer without any parameters.
-
-# (8 points) The Hand class has one public instance variable, cards, 
-# for storing a list of up to 13 cards. Additionally, you must have the required constants,
-#  as can be seen from the attached programs.
-
-# (8 points) __str__(): 
-# The string representation for an instance of a Hand consists of single line containing
-#  the strings representation of each card (with a single space after each card). 
-# If the hand is empty, the string “Empty” is returned.
-
-# (8 points) add_card(card): Adds the given card to the hand. If the hand is full, then nothing happens.
 from card import Card
 class Hand:
     NUMBER_OF_CARDS = 13
 
     def __init__(self):
-        self.hand = []
+        self.cards = []
 
     def __str__(self):
-        show_cards = ""
-        for card in self.hand:
-            if card == self.hand[len(self.hand) - 1]:
-                show_cards = show_cards + card.__str__()
+        '''Read'em and weep. 
+        This function returns a string containing all the cards in the hand'''
+        show_hand = ""
+        for card in self.cards:
+            if card == self.cards[len(self.cards) - 1]:
+                show_hand = show_hand + card.__str__()
             else:
-                show_cards = show_cards + f"{card.__str__() :>3}" + " "
+                show_hand = show_hand + f"{card.__str__() :>3}" + " "
         
-        if self.hand == None:
-            show_cards = "Empty"
-        return show_cards
+        if self.cards == None:
+            show_hand = "Empty"
+        return show_hand
     
     def add_card(self, card : Card):
-        self.hand.append(card)
+        '''This function adds a Card object to the hand'''
+        self.cards.append(card)
 
 
 
 # def main():
-#     hand = Hand()
-#     print(hand)
+#     cards = Hand()
+#     print(cards)
 
 #     ace_of_spades = Card('A', 'S')
-#     hand.add_card(ace_of_spades)
-#     print(hand)
+#     cards.add_card(ace_of_spades)
+#     print(cards)
 #     print(ace_of_spades.__str__())
 #     ace_of_hearts = Card('A', 'H')
-#     hand.add_card(ace_of_hearts)
-#     print(hand)
+#     cards.add_card(ace_of_hearts)
+#     print(cards)
 
 
 # main()
