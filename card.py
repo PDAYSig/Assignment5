e51eb8d7b8dacd3afddb2bdd15e714542de967a9
PDAYSig <patrikdagur@gmail.com>
2025-11-05 16:42:03 +0000

    fixed the return in __str__ no longer returns an extra ' '

--- card.py
+++ card.py
@@ -2,7 +2,7 @@ from typing import Union
 class Card:
     ACE_VALUE = 15 # Since the ace has a different value in different games, We make its value a constant for scalability
 
-    FACE_CARDS = {
+    FACE_CARDS = { # put the face cards in a dictionary for easy access
         'J' : 11,
         'Q' : 12,
         'K' : 13,
@@ -10,9 +10,9 @@ class Card:
         }
     def __init__(self, rank : Union[str, int], suit : str):
         self.suit = suit
+
         '''If we cannot make the rank an integer, it must mean that the card is
             a Jack, Queen, King, or Ace so we must assign them values specially'''
-
         try:
             self.rank = int(rank)
         except ValueError:
@@ -21,14 +21,15 @@ class Card:
 
 
     def __str__(self):
-        if self.rank >= 11:
+        if self.rank < 2 or self.rank > 10:
+            # If ace value is set to 1, as it is in many card games we account for values less than 2
             target_rank = self.rank
             for letter_value, numerical_value in Card.FACE_CARDS.items():
                 if numerical_value == target_rank:
                     print_rank = letter_value
         else:
             print_rank = self.rank
-
+        
         str_return = f'{print_rank}{self.suit}'
         return str_return.rjust(3)
 
